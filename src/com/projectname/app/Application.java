21111ce2970f7ba4b863be4bf5d9aec11be579d9
Mordecai Yiadom <mordeyiadom@gmail.com>
2024-11-22 11:11:46 -0500

    Update Application.java

--- src/com/projectname/app/Application.java
+++ src/com/projectname/app/Application.java
@@ -12,16 +12,23 @@ public class Application
 
     public static Application instance() {return INSTANCE;}
 
-    public static void launch(String[] args)
+    public static Application launch(String[] args)
     {
         //Create Application instance
        if(INSTANCE == null)
            INSTANCE = new Application();
 
        System.out.println("Application has started");
+       return INSTANCE;
     }
 
     public static void terminate()
     {
+        INSTANCE = null;
+
+        System.out.println("Application has been terminated");
     }
+
+    private void kill()
+    {}
 }
