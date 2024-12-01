84c4aef2edcec18c4efcbf9e14a852f2810145a8
Mordecai Yiadom <mordeyiadom@gmail.com>
2024-11-30 23:35:55 -0500

    Add FileManager.java

---
+++ src/com/projectname/app/FileManager.java
@@ -0,0 +1,21 @@
+package com.projectname.app;
+
+/*
+ * Manages file io, directory create, and all other file related operations for the application
+ */
+
+import java.io.File;
+
+public class FileManager
+{
+    protected boolean createHomeDirectory()
+    {
+        return false;
+    }
+
+    protected boolean writeToFile(File file)
+    {
+        return false;
+    }
+
+}
