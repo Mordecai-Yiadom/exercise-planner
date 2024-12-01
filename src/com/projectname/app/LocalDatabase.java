6aa0e1d7d6b6f986d969772f3d01e3e39cf2b202
Mordecai Yiadom <mordeyiadom@gmail.com>
2024-11-30 23:28:19 -0500

    Add LocalDatabase.java

---
+++ src/com/projectname/app/LocalDatabase.java
@@ -0,0 +1,23 @@
+package com.projectname.app;
+
+import com.projectname.app.exercise.*;
+import java.util.Hashtable;
+
+public class LocalDatabase
+{
+    private Hashtable<ExerciseType, Exercise> EXERCISE_DATATABLE;
+
+    protected LocalDatabase()
+    {
+        EXERCISE_DATATABLE = new Hashtable();
+        loadWorkoutPlans();
+        loadExercises();
+    }
+
+    protected void loadWorkoutPlans()
+    {}
+
+    protected void loadExercises()
+    {}
+
+}
