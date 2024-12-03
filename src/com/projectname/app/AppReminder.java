5035369e9be27906c6b20efa3f3a8c115a6b56fd
Mordecai Yiadom <mordeyiadom@gmail.com>
2024-12-03 10:58:50 -0500

    Update

--- src/com/projectname/app/AppReminder.java
+++ src/com/projectname/app/AppReminder.java
@@ -1,14 +1,22 @@
 package com.projectname.app;
 
 import java.text.DateFormat;
+import java.time.*;
 import java.util.Calendar;
 import java.util.Date;
 
 public class AppReminder
 {
+    private LocalDateTime dateTime;
+
+    protected AppReminder(LocalDateTime dateTime)
+    {
+        this.dateTime = dateTime;
+    }
 
 
-    protected AppReminder()
+
+    public enum Purpose
     {
 
     }
--- src/com/projectname/app/ReminderManager.java
+++ src/com/projectname/app/ReminderManager.java
@@ -1,15 +1,23 @@
 package com.projectname.app;
 
+import java.util.*;
+
 public class ReminderManager
 {
     private Thread REMINDER_THREAD;
 
+    LinkedList<AppReminder> CURRENT_REMINDERS;
+
     protected ReminderManager()
     {
         REMINDER_THREAD = new Thread();
-
+        CURRENT_REMINDERS = new LinkedList<>();
     }
 
-
+    public AppReminder pushReminder(AppReminder reminder)
+    {
+        CURRENT_REMINDERS.remove(reminder);
+        return reminder;
+    }
 
 }
