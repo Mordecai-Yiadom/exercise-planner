d0eb85c1cb6206649df5db458972f2ad53c0b360
Alexande-Do <alexandermdo73@gmail.com>
2024-12-09 11:16:03 -0500

    added class for the database functions (NEED CHECK.)

--- src/com/projectname/app/user/Questionnaire.java
+++ src/com/projectname/app/user/Questionnaire.java
@@ -11,9 +11,46 @@ public class Questionnaire
         return null;
     }
 
+
+    private String [][] answerChoices = {
+
+            {"Losing weight", "Building muscle", "Improve endurance", "Personal growth",
+                    "Maintaining health", "IDK", "Aesthetic", "Something else"},
+
+            /*
+            {"Beginner - little to no experience with structured workouts",
+            "Intermediate - Some experience with workouts but not consistent",
+            "Advanced - consistent workouts with experience in specific training types"},
+             */
+
+            {"Home", "Gym", "Outdoors", "No Preference"},
+
+            {"Dumbbells", "Barbells", "Resistance bands", "Yoga mat",
+                    "Cardio machines (e.g., treadmill, bike)", "Bodyweight"
+                    /*"Other (please specify)"
+                     */},
+    };
+
+
     public enum Question
     {
         //put questions here;
+        QUESTION_1("Do you have any medical conditions or injuries?"), // if yes output disclaimer > prompt to continue
+
+
+        QUESTION_2("What is your main goal?"),
+
+        QUESTION_3("How would you describe your fitness level?"),
+                //"What do you want to focus on?",
+
+        QUESTION_4("How many times a week can you dedicate or want to dedicate?"),
+        QUESTION_5("How much time can you spend on each session?"),
+        QUESTION_N("What's your preferred intensity?"),
+                //"What kind of workouts do you enjoy?",
+
+        QUESTION_6("Where do you prefer working out?"),
+
+        QUESTION_7("What equipment do you have access to? (Select all that apply)")
         ;
 
         private String question;
