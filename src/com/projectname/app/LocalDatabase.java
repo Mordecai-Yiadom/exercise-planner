44c274e5b063459311d69d2d181df0b19c961e60
Mordecai Yiadom <mordeyiadom@gmail.com>
2024-12-11 18:09:57 -0500

    Update LocalDatabase

--- src/com/projectname/app/LocalDatabase.java
+++ src/com/projectname/app/LocalDatabase.java
@@ -7,12 +7,12 @@ import java.util.*;
 public class LocalDatabase implements Serializable
 {
     private Hashtable<ExerciseType, Exercise> EXERCISE_DATATABLE;
-    private ArrayList<WorkoutPlan> WORKOUT_PLAN_DATA_LIST;
+    private HashSet<WorkoutPlan> WORKOUT_PLAN_DATA_SET;
 
     protected LocalDatabase()
     {
         EXERCISE_DATATABLE = new Hashtable();
-        WORKOUT_PLAN_DATA_LIST = new ArrayList<>();
+        WORKOUT_PLAN_DATA_SET = new HashSet<>();
     }
 
     public boolean addExercise(Exercise exercise)
@@ -37,17 +37,41 @@ public class LocalDatabase implements Serializable
 
     public boolean addWorkoutPlan(WorkoutPlan workoutPlan)
     {
-        if(workoutPlan != null) return WORKOUT_PLAN_DATA_LIST.add(workoutPlan);
+        if(workoutPlan != null) return WORKOUT_PLAN_DATA_SET.add(workoutPlan);
         return false;
     }
 
     public boolean removeWorkoutPlan(WorkoutPlan workoutPlan)
     {
-        if(workoutPlan != null) return WORKOUT_PLAN_DATA_LIST.remove(workoutPlan);
+        if(workoutPlan != null) return WORKOUT_PLAN_DATA_SET.remove(workoutPlan);
         return false;
+    }
+
+    public Set<Exercise> getExercises(ExerciseType type)
+    {
+        Set<Exercise> exerciseSet = new HashSet<>();
+        for(Exercise exercise : EXERCISE_DATATABLE.values())
+            if(exercise.getType().equals(type)) exerciseSet.add(exercise);
+
+        return exerciseSet;
+    }
+
+    public Set<Exercise> getExercises()
+    {
+        return (Set<Exercise>) EXERCISE_DATATABLE.values();
+    }
+
+    public Set<WorkoutPlan> getWorkoutPlans()
+    {
+        try {return (Set<WorkoutPlan>) WORKOUT_PLAN_DATA_SET.clone();}
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return null;
+        }
     }
 
     public int exerciseDatatableSize() {return EXERCISE_DATATABLE.size();}
 
-    public int workoutPlanDataListSize() {return WORKOUT_PLAN_DATA_LIST.size();}
+    public int workoutPlanDataListSize() {return WORKOUT_PLAN_DATA_SET.size();}
 }
