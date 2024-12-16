70b3da60fff09215d59b65f0b19970e10ee91015
Mordecai Yiadom <mordeyiadom@gmail.com>
2024-12-16 09:49:43 -0500

    Update CreateExerciseWindow

--- src/com/projectname/app/ui/CreateExerciseWindow.java
+++ src/com/projectname/app/ui/CreateExerciseWindow.java
@@ -1,14 +1,23 @@
 package com.projectname.app.ui;
 
+import com.projectname.app.exercise.*;
+
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicRadioButtonUI;
+import javax.swing.plaf.basic.BasicTextFieldUI;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class CreateExerciseWindow extends JDialog
 {
+    private Exercise exercise;
+
+    private ButtonGroup intesityButtonGroup;
+    private ButtonGroup infoButtonGroup;
+
     protected CreateExerciseWindow()
     {
         super(AppUIManager.window(), "Create New Exercise");
-
         init();
         initComponents();
         setVisible(true);
@@ -24,20 +33,38 @@ public class CreateExerciseWindow extends JDialog
 
     private void initComponents()
     {
+        exercise = new Exercise();
         getContentPane().setLayout(new GridLayout(3,1));
+        intesityButtonGroup = new ButtonGroup();
+        infoButtonGroup = new ButtonGroup();
 
         JPanel namePanel = createBackPanel(new FlowLayout(FlowLayout.LEFT), createLabel("Name: "), createTextField());
         getContentPane().add(namePanel);
 
-    }
+        JPanel intensityPanel = createBackPanel(new FlowLayout(FlowLayout.LEFT), createLabel("Intensity: "),
+                createRadioButton("Low", intesityButtonGroup, (e)->{exercise.setIntensity(Exercise.Intensity.LOW);}),
+                createRadioButton("Medium", intesityButtonGroup, (e)->{exercise.setIntensity(Exercise.Intensity.MEDIUM);}),
+                createRadioButton("High", intesityButtonGroup, (e)->{exercise.setIntensity(Exercise.Intensity.LOW);}));
+        getContentPane().add(intensityPanel);
 
+        JPanel infoPanel = createBackPanel(new FlowLayout(FlowLayout.LEFT), createLabel("Info: "),
+                createRadioButton("Reps", infoButtonGroup, null),
+                createRadioButton("Distance", infoButtonGroup, null),
+                createRadioButton("Duration", infoButtonGroup, null));
+        getContentPane().add(infoPanel);
+    }
 
     private JTextField createTextField()
     {
         JTextField textField = new JTextField();
         textField.setFont(new Font(AppUIManager.FONT, Font.BOLD, 20));
         textField.setSize(100,50);
-        textField.setPreferredSize(new Dimension(200,25));
+        textField.setPreferredSize(new Dimension(200,35));
+        textField.setBackground(AppUIManager.MENU_BACKGROUND_COLOR);
+        textField.setForeground(Color.WHITE);
+        textField.setCaretColor(Color.WHITE);
+        textField.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+        textField.setUI(new BasicTextFieldUI());
         return textField;
     }
 
@@ -55,5 +82,19 @@ public class CreateExerciseWindow extends JDialog
         label.setFont(new Font(AppUIManager.FONT, Font.BOLD, 20));
         label.setForeground(Color.WHITE);
         return label;
+    }
+
+    private JRadioButton createRadioButton(String text, ButtonGroup group, ActionListener actionListener)
+    {
+        JRadioButton button = new JRadioButton(text);
+        button.setBackground(Toolbar.BACKGROUND_COLOR);
+        button.setFont(new Font(AppUIManager.FONT, Font.BOLD, 20));
+        button.setForeground(Color.WHITE);
+        button.addActionListener(actionListener);
+        button.setUI(new BasicRadioButtonUI());
+        button.setFocusable(false);
+        if(group != null) group.add(button);
+
+        return button;
     }
 }
