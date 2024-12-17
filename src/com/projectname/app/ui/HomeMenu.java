a1b2dd0d7c00ba1bc803b42b60bf895f771b7b8e
Mordecai Yiadom <mordeyiadom@gmail.com>
2024-12-16 21:48:38 -0500

    Update HomeMenu

--- src/com/projectname/app/ui/HomeMenu.java
+++ src/com/projectname/app/ui/HomeMenu.java
@@ -1,9 +1,14 @@
 package com.projectname.app.ui;
 
+import com.projectname.app.Application;
+import com.projectname.app.Schedule;
 import com.projectname.app.exercise.WorkoutPlan;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class HomeMenu extends JPanel implements AppMenu
 {
@@ -13,12 +18,19 @@ public class HomeMenu extends JPanel implements AppMenu
     private JPanel CENTER_ROOT_PANE;
     private JLabel DAY_LABEL, TIME_LABEL;
     private FlowLayout CENTER_ROOT_PANE_LAYOUT;
+    private DayOfWeek DAY;
     private static final int DEFAULT_V_GAP = (int) (AppUIManager.SCREEN_HEIGHT/4.5);
+
+    protected HomeMenu(DayOfWeek day)
+    {
+        this.DAY = day;
+        init();
+        initComponents();
+    }
 
     protected HomeMenu()
     {
-        init();
-        initComponents();
+        this(DayOfWeek.MONDAY);
     }
 
     private void init()
@@ -37,33 +49,43 @@ public class HomeMenu extends JPanel implements AppMenu
 
     private void initComponents()
     {
-        //Create Set label
-        WORKOUT_PLAN_UI = new WorkoutPlanUI(new WorkoutPlan("WorkoutPlan 1"));
-
-        //Init Day and Time Label
-        DAY_LABEL = new LabelFactory().createTextLabel(LabelFactory.GenericType.HOME_MENU_DAY_LABEL, "Monday");
-        TIME_LABEL = new LabelFactory().createTextLabel(LabelFactory.GenericType.HOME_MENU_TIME_LABEL, "3:30PM");
-
-        DAY_LABEL.setHorizontalTextPosition(SwingConstants.CENTER);
-        TIME_LABEL.setHorizontalTextPosition(SwingConstants.CENTER);
-
-        JPanel dayTimePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        dayTimePanel.setBackground(AppUIManager.MENU_BACKGROUND_COLOR);
-        dayTimePanel.add(DAY_LABEL);
-        dayTimePanel.add(TIME_LABEL);
+        Schedule schedule = Application.instance().getUserSchedule();
+        WorkoutPlan workoutPlan = schedule.getWorkoutPlan(DAY);
 
         JPanel centerPanel = new JPanel();
         centerPanel.setBackground(AppUIManager.MENU_BACKGROUND_COLOR);
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
-        centerPanel.add(dayTimePanel);
-        centerPanel.add(WORKOUT_PLAN_UI);
+        DAY_LABEL = new LabelFactory().createTextLabel(LabelFactory.GenericType.HOME_MENU_DAY_LABEL, DAY.name());
+
+        if(workoutPlan == null)
+        {
+            JLabel messageLabel = createLabel("Nothing planned this day.");
+            centerPanel.add(DAY_LABEL);
+            centerPanel.add(messageLabel);
+        }
+        else
+        {
+            //Create Set label
+            WORKOUT_PLAN_UI = new WorkoutPlanUI(schedule.getWorkoutPlan(DAY));
+
+            //Init Day and Time Label
+            DAY_LABEL = new LabelFactory().createTextLabel(LabelFactory.GenericType.HOME_MENU_DAY_LABEL, DAY.name());
+            TIME_LABEL = new LabelFactory().createTextLabel(LabelFactory.GenericType.HOME_MENU_TIME_LABEL,
+                    "(" + schedule.getScheduledTime(DAY).toString() + ")");
+            DAY_LABEL.setHorizontalTextPosition(SwingConstants.CENTER);
+            TIME_LABEL.setHorizontalTextPosition(SwingConstants.CENTER);
+
+            JPanel dayTimePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            dayTimePanel.setBackground(AppUIManager.MENU_BACKGROUND_COLOR);
+            dayTimePanel.add(DAY_LABEL);
+            dayTimePanel.add(TIME_LABEL);
+
+            centerPanel.add(dayTimePanel);
+            centerPanel.add(WORKOUT_PLAN_UI);
+        }
 
         CENTER_ROOT_PANE.add(centerPanel);
-
-        //CENTER_ROOT_PANE.add(DAY_LABEL);
-        //CENTER_ROOT_PANE.add(TIME_LABEL);
-       // CENTER_ROOT_PANE.add(WORKOUT_PLAN_UI);
 
         //Init Buttons
         ButtonFactory factory = new ButtonFactory();
@@ -73,11 +95,24 @@ public class HomeMenu extends JPanel implements AppMenu
         add(BACKWARD_BUTTON);
         BACKWARD_BUTTON.setBounds(0, (AppUIManager.SCREEN_HEIGHT/2) - 25,
                 BACKWARD_BUTTON.getWidth(), BACKWARD_BUTTON.getHeight());
+        BACKWARD_BUTTON.addActionListener((e)->
+        {AppUIManager.window().displayMenu(new HomeMenu(DAY.minus(1)));});
 
         FORWARD_BUTTON = factory.createIconButton(ButtonFactory.GenericType.HOME_MENU_ARROW_BUTTON,
                 "angle-circle-right.png",null);
         add(FORWARD_BUTTON);
         FORWARD_BUTTON.setBounds(AppWindow.MENU_VIEW_PORT.width - 50, (AppUIManager.SCREEN_HEIGHT/2) - 25,
                 FORWARD_BUTTON.getWidth(), FORWARD_BUTTON.getHeight());
+        FORWARD_BUTTON.addActionListener((e)->
+        {AppUIManager.window().displayMenu(new HomeMenu(DAY.plus(1)));});
+    }
+
+    private JLabel createLabel(String text)
+    {
+        JLabel label = new JLabel(text);
+        label.setForeground(Color.DARK_GRAY);
+        label.setBackground(AppUIManager.MENU_BACKGROUND_COLOR);
+        label.setFont(new Font(AppUIManager.FONT, Font.BOLD, 30));
+        return label;
     }
 }
